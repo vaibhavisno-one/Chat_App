0c549d13ae6f70fc97a4945e3c3bd7c273bc5680
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-02-22 14:56:30 +0530

    Revert "resolve image upload"
    This reverts commit 248d2b8603b4ebb805eaf49492c3c0c42935faba.

--- Backend/src/controllers/message.controller.js
+++ Backend/src/controllers/message.controller.js
@@ -35,40 +35,6 @@ export const getMessages = async (req, res) => {
   }
 };
 
-// export const sendMessage = async (req, res) => {
-//   try {
-//     const { text, image } = req.body;
-//     const { id: receiverId } = req.params;
-//     const senderId = req.user._id;
-
-//     let imageUrl;
-//     if (image) {
-//       // Upload base64 image to cloudinary
-//       const uploadResponse = await cloudinary.uploader.upload(image);
-//       imageUrl = uploadResponse.secure_url;
-//     }
-
-//     const newMessage = new Message({
-//       senderId,
-//       receiverId,
-//       text,
-//       image: imageUrl,
-//     });
-
-//     await newMessage.save();
-
-//     const receiverSocketId = getReceiverSocketId(receiverId);
-//     if (receiverSocketId) {
-//       io.to(receiverSocketId).emit("newMessage", newMessage);
-//     }
-
-//     res.status(201).json(newMessage);
-//   } catch (error) {
-//     console.log("Error in sendMessage controller: ", error.message);
-//     res.status(500).json({ error: "Internal server error" });
-//   }
-// };
-
 export const sendMessage = async (req, res) => {
   try {
     const { text, image } = req.body;
@@ -77,14 +43,6 @@ export const sendMessage = async (req, res) => {
 
     let imageUrl;
     if (image) {
-      // Estimate size of base64 string
-      const estimatedSize = (image.length * 3) / 4 - (image.indexOf('=') > 0 ? image.length - image.indexOf('=') : 0);
-      const maxSize = 5 * 1024 * 1024; // 5 MB
-
-      if (estimatedSize > maxSize) {
-        return res.status(400).json({ error: "Image size must be less than 5 MB" });
-      }
-
       // Upload base64 image to cloudinary
       const uploadResponse = await cloudinary.uploader.upload(image);
       imageUrl = uploadResponse.secure_url;
