da6e58cd5adc4d2859a8de12ed1f557e7988e669
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-02-22 14:55:48 +0530

    Revert "resolve"
    This reverts commit c965978acef8de769d719341885873b602efd551.

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,46 +1,3 @@
-// import express from "express";
-// import dotenv from "dotenv";
-// import cookieParser from "cookie-parser";
-// import cors from "cors";
-
-// import path from "path";
-
-// import { connectDB } from "./lib/db.js";
-
-// import authRoutes from "./routes/auth.route.js";
-// import messageRoutes from "./routes/message.route.js";
-// import { app, server } from "./lib/socket.js";
-
-// dotenv.config();
-
-// const PORT = process.env.PORT;
-// const __dirname = path.resolve();
-
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(
-//   cors({
-//     origin: "http://localhost:5173",
-//     credentials: true,
-//   })
-// );
-
-// app.use("/api/auth", authRoutes);
-// app.use("/api/messages", messageRoutes);
-
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../Frontend/dist")));
-
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../Frontend", "dist", "index.html"));
-//   });
-// }
-
-// server.listen(PORT, () => {
-//   console.log("server is running on PORT:" + PORT);
-//   connectDB();
-// });
-
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
@@ -59,9 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-// Middleware
-app.use(express.json({ limit: "10mb" })); // Increase payload size limit
-app.use(express.urlencoded({ limit: "10mb", extended: true })); // Handle larger URL-encoded payloads
+app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
@@ -70,7 +25,6 @@ app.use(
   })
 );
 
-// Routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
@@ -82,7 +36,6 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-// Start Server
 server.listen(PORT, () => {
   console.log("server is running on PORT:" + PORT);
   connectDB();
