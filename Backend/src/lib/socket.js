437a443784e18de5ffa7fb2a24ab34a9e959eb2c
vaibhav kumar <vaibhav.messiah@gmail.com>
2025-06-17 10:59:40 +0530

    Team issue

--- Backend/src/lib/socket.js
+++ Backend/src/lib/socket.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: ["http://localhost:3000", "http://127.0.0.1:3000"], // Frontend URL
+    origin: ["http://localhost:3000", "http://127.0.0.1:3000"],
     methods: ["GET", "POST"],
   },
 });
